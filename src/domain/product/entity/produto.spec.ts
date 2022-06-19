919a42e758ccac990091ccbbe23bcb364c2fe936
Bruno Gabriel <bruno.glm_@hotmail.com>
2022-06-18 22:44:52 -0300

    adjusts review

--- src/domain/product/entity/produto.spec.ts
+++ src/domain/product/entity/produto.spec.ts
@@ -1,22 +1,36 @@
 import Product from './product'
+import { ValidationError } from 'yup'
 
 describe('Product unit tests', () => {
   it('should throw error when id is empty', () => {
     expect(() => {
-      const product = new Product('', 'Product 1', 100)
+      new Product('', 'Product 1', 100)
     }).toThrowError()
   })
 
   it('should throw error when name is empty', () => {
     expect(() => {
-      const product = new Product('123', '', 100)
+      new Product('123', '', 100)
     }).toThrowError()
   })
 
   it('should throw error when price is less than zero', () => {
     expect(() => {
-      const product = new Product('123', 'Name', -1)
+      new Product('123', 'Name', -1)
     }).toThrowError()
+  })
+
+  it('should throw error when price is less than zero and name is empty', () => {
+    try {
+      new Product('123', '', -1)
+    } catch (error) {
+      const errorsExpected = [
+        { context: 'product', message: 'Name is required' },
+        { context: 'product', message: 'Price must be greater than zero' },
+      ]
+      const e = error as ValidationError
+      expect(e.errors).toStrictEqual(errorsExpected)
+    }
   })
 
   it('should change name', () => {
