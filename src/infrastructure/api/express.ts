c652587b03235f564fe30723d5912a06a542342a
Bruno Gabriel <bruno.glm_@hotmail.com>
2022-06-25 18:38:34 -0300

    desafio api de products

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -2,10 +2,13 @@ import express, { Express } from 'express'
 import { Sequelize } from 'sequelize-typescript'
 import CustomerModel from '../customer/repository/sequelize/customerModel'
 import { customerRoute } from './routes/customer-route'
+import { productRoute } from './routes/product-route'
+import ProductModel from '../product/repository/sequelize/productModel'
 
 export const app: Express = express()
 app.use(express.json())
 app.use('/customers', customerRoute)
+app.use('/products', productRoute)
 
 export let sequelize: Sequelize
 
@@ -16,6 +19,7 @@ async function setupDb() {
     logging: false,
   })
   await sequelize.addModels([CustomerModel])
+  await sequelize.addModels([ProductModel])
   await sequelize.sync()
 }
 setupDb()
